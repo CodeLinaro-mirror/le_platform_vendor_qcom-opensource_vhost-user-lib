c3cf261dabc3cea853a363ba6c28e0983a7cbdac
Conghui Chen <quic_conghuic@quicinc.com>
2024-02-21 15:52:12 +0800

    Tiny fixes for the initialize process
    1. VHOST_USER_SUPPORT_FEAT is the vhost user library related feature, the
    vhost user device only need to care their own features, this library
    feature will be added to the feature set return to vhost user FE.
    
    2. Fix the wrong index when initialize a vring.
    
    Change-Id: I9450d09b1cf5e8320b5319a53b40089aa2db6217
    Signed-off-by: Conghui Chen <quic_conghuic@quicinc.com>

--- vhost-user/src/vhost_user.c
+++ vhost-user/src/vhost_user.c
@@ -63,7 +63,7 @@ alloc_vring_queue(struct vhost_user_dev *dev, uint32_t vring_idx)
             pr_err("failed to allocate memory for vring %u.\n", i);
             return -1;
         }
-        init_vring_queue(dev, vq, vring_idx);
+        init_vring_queue(dev, vq, i);
         dev->virtqueue[i] = vq;
     }
 
@@ -120,10 +120,10 @@ vhost_user_get_features(struct vhost_user_dev *dev,
     if (dev->dev_ops->get_features)
         features = dev->dev_ops->get_features(dev);
 
-    ctx->msg.payload.u64 = features & VHOST_USER_SUPPORT_FEAT;
+    ctx->msg.payload.u64 = features | VHOST_USER_SUPPORT_FEAT;
     ctx->msg.size = sizeof(ctx->msg.payload.u64);
     ctx->fd_num = 0;
-    pr_debug("get features %lx \n", features);
+    pr_debug("get features %lx \n", ctx->msg.payload.u64);
 
     return VHOST_MSG_RESULT_REPLY;
 }
